15ff37c5e40a7be0093e1b367d3560a56c2ae862
Antonio Wu <anwu@uade.edu.ar>
2026-09-08 12:43:19 -0300

    Update PanelDirectoryService.java

--- login-federado/src/main/java/citypass/loginfederado/panel/PanelDirectoryService.java
+++ login-federado/src/main/java/citypass/loginfederado/panel/PanelDirectoryService.java
@@ -96,13 +96,6 @@ public class PanelDirectoryService {
         assertModule(module);
         try {
             DirContextOperations ctx = ldap.lookupContext(personDn(module, uid));
-            return Optional.of(new PersonView(
-                    ctx.getStringAttribute("employeeNumber"),
-                    ctx.getStringAttribute("uid"),
-                    ctx.getStringAttribute("givenName"),
-                    ctx.getStringAttribute("sn"),
-                    ctx.getStringAttribute("mail"),
-                    isDisabled(ctx)));
             // lookup explícito de los atributos de la ficha: pwdAccountLockedTime
             // es operacional y solo se devuelve si se pide por nombre.
             PersonView view = ldap.lookup(personDn(module, uid), PERSON_VIEW_ATTRIBUTES,
